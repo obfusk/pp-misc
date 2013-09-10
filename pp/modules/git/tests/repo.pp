5de25db863e2fcb6f5d86c0513dc0f7869bbb73b
Felix C. Stegerman <flx@obfusk.net>
2013-09-10 15:31:29 +0200

    fix repo test

--- pp/modules/git/tests/repo.pp
+++ pp/modules/git/tests/repo.pp
@@ -1,17 +1,17 @@
-git::repo { "${::TESTDIR}/pp-misc-1":
-  source    => 'https://github.com/obfusk/pp-misc.git',
-  branch    => 'dev',               # TODO
+git::repo { "${::TESTDIR}/pp-misc-git-repo-oc-1":
+  source    => 'https://github.com/obfusk/obfusk.coffee.git',
+  branch    => 'gh-pages',
   log       => on_failure,
 }
 
-git::repo { "${::TESTDIR}/pp-misc-2":
-  source    => 'https://github.com/obfusk/pp-misc.git',
-  branch    => 'dev',
+git::repo { "${::TESTDIR}/pp-misc-git-repo-oc-2":
+  source    => 'https://github.com/obfusk/obfusk.coffee.git',
+  branch    => 'gh-pages',
   pull      => true,
   log_pull  => on_failure,
 }
 
-git::repo { "${::TESTDIR}/pp-misc-3":
-  source    => 'https://github.com/obfusk/pp-misc.git',
-  checkout  => ['foo', '1df4e76'],  # TODO
+git::repo { "${::TESTDIR}/pp-misc-git-repo-gitbak":
+  source    => 'https://github.com/obfusk/gitbak.git',
+  checkout  => ['v0.4.2-branch', 'v0.4.2'],
 }
